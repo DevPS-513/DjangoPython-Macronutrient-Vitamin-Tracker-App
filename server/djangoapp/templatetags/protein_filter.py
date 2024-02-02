c88b9fd9538e36e72d092979c496e613bad41f58
DevPS-513 <max.bethune.waddell@gmail.com>
2024-02-01 22:36:07 -0500

    added getting vitamin by letter

--- server/djangoapp/templatetags/protein_filter.py
+++ server/djangoapp/templatetags/protein_filter.py
@@ -8,3 +8,11 @@ def get_nutrient(food, nutrient_name):
         if nutrient_name in str(nutrient.nutrient.name).lower():
             return nutrient.amount
     return 0
+
+
+@register.filter
+def get_vitamin_by_letter(food, letter):
+    for nutrient in food.foodnutrient_set.all():
+        if ("vitamin" in str(nutrient.nutrient.name).lower())&(letter.lower() in str(nutrient.nutrient.name).lower()):
+            return nutrient.amount
+    return 0
